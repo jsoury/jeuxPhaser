76716cc17805b763c063a84352b3e941020f751a
Jonathan <jonathan.soury@gmail.com>
2020-11-11 00:13:32 +0100

    Ajout des chronos page kuru game

--- src/client/js/kuru/gestion_chrono.js
+++ src/client/js/kuru/gestion_chrono.js
@@ -1,6 +1,8 @@
 var gestionChrono = {
   chronoSend: false,
   chronos: null,
+  button: null,
+  isSort: false,
 
   getChrono: function (playerName, level, chrono) {
     if (jeu.win) {
@@ -42,6 +44,72 @@ var gestionChrono = {
       tabChrono.push(chronos.chrono[chrono]);
     }
     this.chronos = tabChrono;
+    gestionChrono.afficherChronos();
+  },
+  afficherChronos: function () {
+    let chronos = this.chronos;
+    chronos = chronos.sort(this.triByLevelAndChrono);
+    let txt = "";
+    txt += `<table id='tabChrono' class='table table-striped w-50 mx-auto'>
+      <thead>
+          <tr>
+          <th><button type='submit' id='buttonTrieTabPlayerName' class="btn btn-link">Pseudo</button></th>
+          <th>level</th>
+          <th>Chrono</th>
+        </tr>
+      <tbody>`;
+    for (let ligne in chronos) {
+      txt += `<tr>
+        <td>${chronos[ligne].playerName}</td>
+        <td>${chronos[ligne].level}</td>
+        <td>${chronos[ligne].chrono}</td>
+      </tr>`;
+    }
+    txt += "</tbody></table>";
+    $("#chrono").html(txt);
+
+    $("#buttonTrieTabPlayerName").on("click", () => {
+      !this.isSort ? (this.isSort = true) : (this.isSort = false);
+      this.chronoByName();
+    });
+  },
+  chronoByName: function () {
+    let chronos;
+    let chronosNoOrder = this.chronos;
+    let chronosOrderByName = [...chronosNoOrder].sort(this.triByNom);
+
+    this.isSort ? (chronos = chronosOrderByName) : (chronos = chronosNoOrder);
+
+    for (let ligne in chronos) {
+      let txt = "";
+      txt += `<tr>
+        <td>${chronos[ligne].playerName}
+        <td>${chronos[ligne].level}</td>
+        <td>${chronos[ligne].chrono}</td>
+      </tr>`;
+      $(`#tabChrono tr:eq(${parseInt(ligne) + 1})`).replaceWith($(txt));
+    }
+  },
+  triByLevelAndChrono: function (a, b) {
+    var critere,
+      criteres = ["level", "chrono"];
+    for (critere in criteres) {
+      if (a[criteres[critere]] != b[criteres[critere]]) {
+        return a[criteres[critere]] > b[criteres[critere]] ? 1 : -1;
+      }
+    }
+    return 0;
+  },
+  triByNom: function (a, b) {
+    var critere,
+      criteres = "playerName";
+    const first = a[criteres].toUpperCase();
+    const second = b[criteres].toUpperCase();
+    if (first != second) {
+      return first > second ? 1 : -1;
+    }
+
+    return 0;
   },
 };
 const myRequest = new Request("/chrono");
